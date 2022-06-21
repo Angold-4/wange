d668db10b543dca7ccea195c3e42021781447a6e
Angold-4 <wangold4w@gmail.com>
2022-06-21 17:30:02 +0800

    init, get the window size of terminal

---
+++ cange/cange.cpp
@@ -0,0 +1,89 @@
+/* 
+ * cange -- a very small, simple text editor
+ * -----------------------------------------------------------------------------
+ * Copyright (c) 2022 Angold Wang <wangold4w at gmail dot com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "./cange.hpp"
+
+static struct cangeConfig Config;
+
+
+
+void loadConfig(void) {
+
+};
+
+
+/* Try to get the number of cols and rows in the current terminal.
+ * Using ioctl(), returns 0 on succress, -1 on error. */
+int getWindowSize(int ifd, int ofd, int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(1, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0 || ws.ws_row == 0) {
+    // ioctl failed, just return;
+    return -1;
+  }
+
+  *cols = ws.ws_col;
+  *rows = ws.ws_row;
+
+  return 0;
+}
+
+
+void updateWindowSize(void) {
+  if (getWindowSize(STDIN_FILENO, STDOUT_FILENO, &Config.srows, &Config.scols) == -1) {
+    std::cerr << "Unable to get the shell screen size (using ioctl)! \n";
+    exit(1);
+  }
+}
+
+
+// load the configure file, then do initialization
+void init(void) {
+  Config.cx = 0;
+  Config.cy = 0;
+  Config.offcol = 0;
+  Config.offrow = 0;
+  Config.numrows = 0;
+  Config.dirty = 0;
+  Config.filename = NULL;
+  Config.syntax = NULL;
+  updateWindowSize();
+
+#ifdef DEBUG
+  std::cout << "rows: " << Config.srows << std::endl;
+  std::cout << "cols: " << Config.scols << std::endl;
+#endif
+}
+
+
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: cange <filename> \n";
+    exit(1);
+  }
+
+  init();
+  loadConfig();
+}
---
+++ cange/cange.hpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <iostream>
+
+#define DEBUG 1;
+
+/* the syntax of current open file */
+struct cangeSyntax {
+  // each char array should end with a explicit NULL
+  char **filetype; // the type of current file, based on its postfix
+  char **keywords; // the highlightable keywords
+  char **single_comment;
+  char **multi_comments;
+};
+
+// a single line of the file that we are currently editing.
+typedef struct crow {
+  int index;       // current row index in the file, zero-based.
+  int size;        // size of the row
+  char *content;   // row content
+  char *renderc;   // content rendered for screen (TABs)
+} crow;
+
+struct cangeConfig {
+  int cx, cy;      // Cursor's current position
+  int offrow;      // Offset of row displayed
+  int offcol;      // Offset of col displayed
+
+  int srows;       // # of rows that we can show
+  int scols;       // # of cols that we can show
+
+  int numrows;     // total rows of this file
+
+  int dirty;       // 1 if there are some unsaved data
+  char *filename;  // open filename
+
+  struct cangeSyntax *syntax;   // Current syntax highlight, or NULL
+  crow *crow;      // rows
+};
+
